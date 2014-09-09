f9dbc8cb17af9608eabe22d301c277e25183a21d
yanpeipan <yanpeipan_82@qq.com>
2014-09-09 16:43:01 +0800

    move submenu to basic config

--- protected/modules/plugin/src/git/git.php
+++ protected/modules/plugin/src/git/git.php
@@ -22,52 +22,55 @@ return array(
 	'description' => 'Project Manager',
 //菜单
 	'menus' => array(
-//主菜单
+		//嵌套子菜单
+		'SUBMENU'=>array(
+				'cfg_value' => '/plugin/git/manage',
+				'cfg_comment' => '项目配置',
+				'cfg_order' => 1,
+				'cfg_pid' => 17,
+			),
+
+		//主菜单
 		'MAINMENU' => array(
 			'icon' => 'fa-heart',
 			'cfg_value' => '/plugin/git/index',
 			'cfg_pid' => 0,
 			'cfg_comment' => '项目管理',
 			'cfg_order' => 6,
-//嵌套子菜单
+			//嵌套子菜单
 			'SUBMENU'=>array(
 				array(
 					'cfg_value' => '/plugin/git/index',
 					'cfg_comment' => '项目列表',
 					'cfg_order' => 1,
-					),
-				array(
-					'cfg_value' => '/plugin/git/manage',
-					'cfg_comment' => '项目配置',
-					'cfg_order' => 1,
-					),
+				     ),
 				),
 			),
 		),
-//sql
-	'execsql' => array(
-		"CREATE TABLE  IF NOT EXISTS  `{{projects}}` (
-			`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-			`name` varchar(45) NOT NULL,
-			`uid` int(11) NOT NULL,
-			`ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-			`remote_url` varchar(255) NOT NULL,
-			`domain` varchar(45) NOT NULL,
-			`status` varchar(45) DEFAULT NULL,
-			`type` varchar(45) NOT NULL,
-			PRIMARY KEY (`id`),
-			UNIQUE KEY `name_UNIQUE` (`name`)
-			) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
+		//sql
+		'execsql' => array(
+				"CREATE TABLE  IF NOT EXISTS  `{{projects}}` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`name` varchar(45) NOT NULL,
+	`uid` int(11) NOT NULL,
+	`ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	`remote_url` varchar(255) NOT NULL,
+	`domain` varchar(45) NOT NULL,
+	`status` varchar(45) DEFAULT NULL,
+	`type` varchar(45) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `name_UNIQUE` (`name`)
+	) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
 
-"CREATE TABLE  IF NOT EXISTS `{{projects_members}}` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+				"CREATE TABLE  IF NOT EXISTS `{{projects_members}}` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`pid` int(10) unsigned NOT NULL,
 	`uid` int(10) unsigned NOT NULL,
 	PRIMARY KEY (`id`),
 	UNIQUE KEY `uniqe` (`pid`,`uid`)
 	) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
 
-"CREATE TABLE  IF NOT EXISTS  `{{repositories}}` (
+	"CREATE TABLE  IF NOT EXISTS  `{{repositories}}` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`name` varchar(45) NOT NULL,
 	`description` text NOT NULL,
@@ -83,8 +86,8 @@ return array(
 	PRIMARY KEY (`id`)
 	) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
 
-"CREATE TABLE  IF NOT EXISTS `{{projects}}` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	"CREATE TABLE  IF NOT EXISTS `{{projects}}` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`name` varchar(45) NOT NULL,
 	`uid` int(11) NOT NULL,
 	`ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -96,8 +99,8 @@ return array(
 	UNIQUE KEY `domain_UNIQUE` (`domain`)
 	) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
 
-"CREATE TABLE   IF NOT EXISTS  `{{virtual_servers}}` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	"CREATE TABLE   IF NOT EXISTS  `{{virtual_servers}}` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
 	`name` varchar(45) NOT NULL,
 	`description` text NOT NULL,
 	`htdocs_path` varchar(3841) NOT NULL,
@@ -111,43 +114,43 @@ return array(
 	PRIMARY KEY (`id`)
 	) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;",
 
-"INSERT IGNORE INTO  `{{virtual_servers}}`
+	"INSERT IGNORE INTO  `{{virtual_servers}}`
 (`id`,
-	`name`,
-	`description`,
-	`htdocs_path`,
-	`nginx_config_path`,
-	`ngixn_bin`,
-	`ip`,
-	`ssh_port`,
-	`url_port`,
-	`url_host`,
-	`url_schema`)
+ `name`,
+ `description`,
+ `htdocs_path`,
+ `nginx_config_path`,
+ `ngixn_bin`,
+ `ip`,
+ `ssh_port`,
+ `url_port`,
+ `url_host`,
+ `url_schema`)
 VALUES
 (1, 'test', 'desc', '/var', '/etc/nginx', 'nginx', '127.0.0.1', 22, 80, 'test.com', 'http');",
 
-"INSERT IGNORE INTO `{{repositories}}`
+	"INSERT IGNORE INTO `{{repositories}}`
 (`id`,
-	`name`,
-	`description`,
-	`root_path`,
-	`apache_group_file`,
-	`apache_user_file`,
-	`ip`,
-	`ssh_port`,
-	`apache_bin`,
-	`url_port`,
-	`htpasswd_bin`,
-	`git_config_path`)
+ `name`,
+ `description`,
+ `root_path`,
+ `apache_group_file`,
+ `apache_user_file`,
+ `ip`,
+ `ssh_port`,
+ `apache_bin`,
+ `url_port`,
+ `htpasswd_bin`,
+ `git_config_path`)
 VALUES
 (1, 'test', 'desc', '/repo', 'group_file', 'user_file', '127.0.0.1', 22, 'apache2', '80', 'htpasswd', 'git');",
 
-//"ALTER TABLE {{admin}} ADD encrypt varchar(255) NOT NULL;",
-),
+	//"ALTER TABLE {{admin}} ADD encrypt varchar(255) NOT NULL;",
+	),
 
 
 
 
 
 
-);
+	);
