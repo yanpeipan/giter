305a68dfd5c3c2e64dcab08165920fba6b0459ed
yan <yanpeipan_82@qq.com>
2014-09-19 14:43:14 +0800

    test

--- protected/modules/admin/extensions/Lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Lib{
-	static public function utf8Substr($str, $from, $len) 
-	{ 
-		return preg_replace('#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$from.'}'. 
-		'((?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){0,'.$len.'}).*#s', 
-		'$1',$str); 
-	}
-}
